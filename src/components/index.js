445c30395028dcbc5cd02d9229f600cf36117f07
bakemonostan <ehizomokhomion@gmail.com>
2022-04-24 13:10:48 -0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import AboutMe from './AboutMe';
+import Card from './Card';
+import ContactMe from './ContactMe';
+import Home from './Home';
+import PortFolio from './PortFolio';
+
+export { AboutMe, Card, ContactMe, Home, PortFolio };
